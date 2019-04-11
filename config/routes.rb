1598b0a56f45f8b7751f562c878d94365baefda9
Josefina <josefina@MacBook-Pro-de-Josefina-2.local>
2019-04-11 00:48:36 -0400

    First commit for lab 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'event/Venues'
+	get 'welcome/user'
+
+	resources :users
+	resources :events
+	resources :event_venues
+	resources :tickets
+	root 'welcome#user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
